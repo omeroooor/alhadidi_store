82771dfabdd6857b840595500acb7802e6ca496e
Omer abd almajeed <omeroooor@gmail.com>
2020-07-18 04:34:20 +0200

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Alhadidi Store',
+    'summary': "Customizing Web store for Alhadidi Store",
+    'version': '1.0',
+    'depends': ['theme_laze'],
+    'data': [  
+        #'data/product_public_categories.xml',
+        'views/assets.xml',
+        'views/templates.xml',
+        #'views/product_view.xml',
+        #'views/megamenu_view.xml',
+        #'views/megamenu_template.xml',
+    ], 
+    'installable': True,
+    'application': True,
+}
---
+++ static/src/js/theme_front.js
@@ -0,0 +1,67 @@
+odoo.define('alhadidi_store.carousel_rtl', function (require) {
+    "use strict";
+
+    var laze_carousel = require('theme_laze.front_js_multi');
+    var animation = require('web_editor.snippets.animation');
+    function initialize_owl(el){
+        var rtl = false;
+        if($('body').hasClass('rtl')) {rtl = true;}
+        console.log(rtl)
+        el.owlCarousel({
+            rtl: rtl,
+            items: 4,
+            margin: 30,
+            navigation: true,
+            pagination: false,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                481: {
+                    items: 2,
+                },
+                768: {
+                    items: 3,
+                },
+                1024: {
+                    items: 4,
+                }
+            }
+
+        })
+    }
+    function destory_owl(el){
+        if(el && el.data('owlCarousel'))
+       {
+        el.data('owlCarousel').destroy();
+        el.find('.owl-stage-outer').children().unwrap();
+        el.removeData();
+        }
+      }
+      animation.registry.s_product_multi_with_header = animation.Class.extend({
+            selector : ".js_multi_collection",
+                start: function (editMode) {
+                var self = this;
+                if (editMode)
+                {
+                //$('.js_multi_collection').addClass("hidden");
+                }
+                if(!editMode){
+                var list_id=self.$target.attr('data-list-id') || false;
+                $.get("/shop/get_multi_tab_content",{'collection_id':list_id}).then(function (data){
+    
+                    if(data){                   
+                        self.$target.empty().append(data);
+                        $(".js_multi_collection").removeClass('hidden');
+                        $('a[data-toggle="tab"]').on('shown.bs.tab', function () {
+                            initialize_owl($(".multi_tab_slider .tab-content .active .multi_slider"));
+                        }).on('hide.bs.tab', function () {
+                        destory_owl($(".multi_tab_slider .tab-content .active .multi_slider"));
+                        });           
+                        initialize_owl($(".multi_tab_slider .tab-content .active .multi_slider"));
+        }
+                });
+                }
+            },
+      });
+});
